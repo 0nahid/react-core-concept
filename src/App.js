19abb8ca8156350caa259e78fdd93ab5dd9234af
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-23 01:07:34 +0600

    added new param

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name = 'Nahid';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>
+        <Person> </Person>
+        </span>
       </header>
     </div>
   );
 }
-
+function Person() {
+return <h1>Welcome to React World</h1>
+}
 export default App;
